94a3899067b9469219647742b9622995679eb7eb
Nadjib Amar <hello@ebabel.eu>
2018-01-30 07:03:50 +0100

    reserved words

--- routes/apps.js
+++ routes/apps.js
@@ -9,10 +9,15 @@ const update = require('../models/apps/update');
 const list = require('../models/apps/list');
 
 const respondJson = require('../utils/respond-json');
+const cleanString = require('../utils/clean-string');
 
 router.post('/', (req, res, next) => {
   if (!req.body || !req.body.name || !req.body.author || !req.body.contact) {
     return respondJson(res, { message: 'Please POST a JSON payload: name (string - name of your app), author (string), contact (string - e-mail or other details to contact you).' }, 400);
+  }
+
+  if (cleanString(req.body.name) === 'apps' || cleanString(req.body.name) === 'banks') {
+    return respondJson(res, { message: 'Please select a name that is not a reserved word in this API.' }, 400);
   }
 
   create(req, res);
